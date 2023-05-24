752de5c05f91b5963f90c184c2e4e41616d30c28
Crowdbotics <engineering@crowdbotics.com>
2023-05-24 18:25:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smartwaiterapp-dev-85877",
+  url: "https://smartwaiterapp-dev-85877.botics.co",
+  api: "https://smartwaiterapp-dev-85877.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
